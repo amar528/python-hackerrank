d4f167f1d6bada8ef9c3b605560288e6a58b94e0
amar528 <amar.om@icloud.com>
2024-05-20 10:04:04 +1000

    Refactor - move

--- findangle.py
+++
@@ -1,6 +0,0 @@
-from math import degrees, atan
-
-ab = int(input())
-bc = int(input())
-mbc = round(degrees(atan(ab / bc)))
-print(f'{mbc}{chr(176)}')
--- prep_kit_1week/test_day1.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-
-from day_1 import *
-
-
-class Test(TestCase):
-    def test_it(self):
-        find_median()
